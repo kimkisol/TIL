a6e804441d641206846ba9bfcf3c9b6a07476f16
kimkisol <kimkisol2@gmail.com>
2022-01-25 21:50:11 +0900

    algo

--- Algorithm/Baekjun/22944_죽음의비.py
+++ Algorithm/Baekjun/22944_죽음의비.py
@@ -1,39 +1,112 @@
 import sys
-# KB / 864ms
+sys.setrecursionlimit(10**6)
+# KB / ms
 # input = sys.stdin.readline
 '''
-출력 초과 => strip() 
-시간 초과 => 각 문자가 어느 위치에 사용됐었는지 판단
 '''
 
-sys.stdin = open('input_6443.txt')
+sys.stdin = open('input_22944.txt')
 
-def dfs(idx):
+# def bfs(node, hp, umbrella):
+#     global min_move
+#
+#     move = 0
+#     Q = [node]
+#
+#     while Q:
+#         r, c = Q.pop(0)
+#         if arr[r][c] == 'U':
+#
+#             umbrella = D
+#         elif umbrella:
+#             dfs(nr, nc, move + 1, hp, umbrella - 1)
+#         elif hp > 1:
+#             dfs(nr, nc, move + 1, hp - 1, umbrella)
+#         else:
+#         move += 1
+#
+#     if move == min_move:
+#         return
+#
+#     if arr[r][c] == 'E':
+#         min_move = move
+#         return
+#
+#     for d in range(4):
+#         nr, nc = r + dr[d], c + dc[d]
+#         if 0 <= nr < N and 0 <= nc < N and move < visited[nr][nc]:
+#             visited[nr][nc] = move + 1
+#             if arr[nr][nc] == 'U':
+#                 dfs(nr, nc, move + 1, hp, D)
+#             elif umbrella:
+#                 dfs(nr, nc, move + 1, hp, umbrella - 1)
+#             elif hp > 1:
+#                 dfs(nr, nc, move + 1, hp - 1, umbrella)
+#
+#
+# T = int(input())
+#
+# for _ in range(T):
+#     N, H, D = map(int, input().split())  # N: 한변의 길이, H: 현재 체력, D: 우산 내구도
+#     arr = [list(input()) for _ in range(N)]  # S: 시작, U: 우산, E: 도착
+#     visited = [[N * N] * N for _ in range(N)]
+#     min_move = 987654321
+#     dr = [0, 1, 0, -1] # 우하좌상
+#     dc = [1, 0, -1, 0]
+#
+#     for i in range(N):
+#         for j in range(N):
+#             if arr[i][j] == 'S':
+#                 visited[i][j] = 0
+#                 bfs((i, j), H, 0)
+#                 break
+#
+#     if min_move == 987654321:
+#         print(-1)
+#     else:
+#         print(min_move)
 
-    if idx == N:
-        word = ''.join(sel)
-        print(word)
-        return
 
-    for i in range(N):
-        if not visited[i] and not used[arr[i]][idx]:
-            visited[i] = 1
-            sel[idx] = arr[i]
-            used[arr[i]][idx] = 1  # POINT) 사용한 문자의 idx 사용 여부 1로 변경
-            dfs(idx + 1)
-            sel[idx] = 0
-            visited[i] = 0
-            if idx != N - 1:  # POINT) 마지막 순서 제외하고 다음 순서의 사용 여부 전부 0으로 초기화
-                for lst in used.values():
-                    lst[idx + 1] = 0
-
-T = int(input())
-
-for _ in range(T):
-    arr = sorted(list(input().strip()))
-    N = len(arr)
-    visited = [0] * N  # arr에서 사용되었는지 여부
-    sel = [0] * N  # 마지막에 출력할 배열
-    used = {key: [0] * N for key in set(arr)}  # 각 문자별(중복x) idx에 사용되었는지 여부
-
-    dfs(0)
+# def dfs(r, c, move, hp, umbrella):
+#     global min_move
+#
+#     if move == min_move:
+#         return
+#
+#     if arr[r][c] == 'E':
+#         min_move = move
+#         return
+#
+#     for d in range(4):
+#         nr, nc = r + dr[d], c + dc[d]
+#         if 0 <= nr < N and 0 <= nc < N and move < visited[nr][nc]:
+#             visited[nr][nc] = move + 1
+#             if arr[nr][nc] == 'U':
+#                 dfs(nr, nc, move + 1, hp, D)
+#             elif umbrella:
+#                 dfs(nr, nc, move + 1, hp, umbrella - 1)
+#             elif hp > 1:
+#                 dfs(nr, nc, move + 1, hp - 1, umbrella)
+#
+#
+# T = int(input())
+#
+# for _ in range(T):
+#     N, H, D = map(int, input().split())  # N: 한변의 길이, H: 현재 체력, D: 우산 내구도
+#     arr = [list(input()) for _ in range(N)]  # S: 시작, U: 우산, E: 도착
+#     visited = [[N * N] * N for _ in range(N)]
+#     min_move = 987654321
+#     dr = [0, 1, 0, -1] # 우하좌상
+#     dc = [1, 0, -1, 0]
+#
+#     for i in range(N):
+#         for j in range(N):
+#             if arr[i][j] == 'S':
+#                 visited[i][j] = 0
+#                 dfs(i, j, 0, H, 0)
+#                 break
+#
+#     if min_move == 987654321:
+#         print(-1)
+#     else:
+#         print(min_move)
