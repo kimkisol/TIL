3b51781d5f72f08cc00f3e65b43e345b841e6452
kimkisol <kimkisol2@gmail.com>
2022-03-17 00:21:29 +0900

    algo

---
+++ Practice Project/TypeScript/inflearn/class-note/1_type-basic.ts
@@ -0,0 +1,19 @@
+// JS 문자열 선언
+let str = 'hello'
+
+// TS 문자열 선언
+let str2: string = 'hello'
+
+// TS 숫자
+let num: number = 10;
+
+// TS 배열
+let arr: Array<number> = [1, 2, 3]
+let heroes: Array<string> = ['Capt', 'Thor', 'Hulk', 10]
+let items: number[] = [1, 2, 3]
+
+// TS: 튜플 (각각의 인덱스에 타입이 정의된 형태)
+let address: [string, number] = ['gangnam', 100]
+
+// TS: 객체
+let obj: object = {};
