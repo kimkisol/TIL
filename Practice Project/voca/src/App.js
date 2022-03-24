79cf7ce85339b7a1790da597a0e0f5c037030d5d
kimkisol <kimkisol2@gmail.com>
2022-03-24 22:18:20 +0900

    algo & react

--- Practice Project/voca/src/App.js
+++ Practice Project/voca/src/App.js
@@ -2,22 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = "Kisol"
+  const naver = {
+    name: "naver",
+    url: "https://naver.com",
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1
+        style={{
+          color: 'white',
+          backgroundColor: 'green',
+        }}
+      >
+        Welcome {name} {30-1}
+      </h1>
+      <a href={naver.url}>{naver.name}</a>
     </div>
   );
 }
